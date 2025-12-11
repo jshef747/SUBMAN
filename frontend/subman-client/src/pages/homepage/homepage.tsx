d78df3c4d32cce4f6b7b5fbb9a17c3d727a14efb
idanyossi <146638614+idanyossi@users.noreply.github.com>
2025-12-11 19:15:25 +0200

    we have a bit of a cheeky homePage text

--- frontend/subman-client/src/pages/homepage/homepage.tsx
+++ frontend/subman-client/src/pages/homepage/homepage.tsx
@@ -1,5 +1,10 @@
+import MainBody from "./components/mainBody/MainBody";
 import "./homepage.css";
 
 export default function HomePage() {
-  return <div className="homepage-container"></div>;
+  return (
+    <div className="homepage-container">
+      <MainBody />
+    </div>
+  );
 }
